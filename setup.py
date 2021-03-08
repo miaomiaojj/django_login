ed868907da98141de782153d2d7d6f95fbfe5021
miaomiaojj <77264750+miaomiaojj@users.noreply.github.com>
2021-03-08 16:22:50 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django_login',
+    version='',
+    packages=['apps', 'apps.register', 'apps.register.migrations', 'login_registration'],
+    url='',
+    license='',
+    author='miao jing',
+    author_email='',
+    description=''
+)
