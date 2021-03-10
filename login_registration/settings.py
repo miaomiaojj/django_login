65d4aa443a0a919c4a5cec987de53d8465e0213b
miaomiaojj <77264750+miaomiaojj@users.noreply.github.com>
2021-03-10 15:47:55 +0800

    Procfile

--- login_registration/settings.py
+++ login_registration/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
